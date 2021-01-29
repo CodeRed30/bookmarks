3c3557304d32692fae43e6ba3e24e4f9bf4ca65b
CodeRed30 <katrinaharradine@gmail.com>
2021-01-29 21:23:43 +0000

    Updates

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -1,10 +1,7 @@
 require './lib/database_connection'
 
 if ENV['ENVIRONMENT'] == 'test'
-  p 'test1'
   DatabaseConnection.setup('bookmark_manager_test')
 else
-  p 'test2'
-  p ENV['ENVIRONMENT']
   DatabaseConnection.setup('bookmark_manager')
 end
